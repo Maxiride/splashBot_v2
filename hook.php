3839cdb7781f3df1eb4246db88155a473b6e8410
Unknown <maxiride@gmail.com>
2017-09-07 16:41:33 +0200

    0

---
+++ hook.php
@@ -0,0 +1,18 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+// Load secret variables
+include 'myVars.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Handle telegram webhook request
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
+    // log telegram errors
+    // echo $e->getMessage();
+}
---
+++ set.php
@@ -0,0 +1,20 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+// Load secret variables
+include 'myVars.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+    // echo $e->getMessage();
+}
