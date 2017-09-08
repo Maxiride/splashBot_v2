2834724c2585a9e10050f88a25517a3cc3931be3
Unknown <maxiride@gmail.com>
2017-09-08 15:52:30 +0200

    secret reset

--- hook.php
+++ hook.php
@@ -30,9 +30,9 @@ if ($ip_dec < $lower_dec || $ip_dec > $upper_dec) {
 
 
 // Secret check
-// if (!isset($_GET['secret']) || $_GET['secret'] !== $secret) {
-//     die("I'm safe =)");
-// }
+if (!isset($_GET['secret']) || $_GET['secret'] !== $secret) {
+    die("I'm safe =)");
+}
 
 
 // Define all IDs of admin users in this array (leave as empty array if not used)
